91a785871c20800ca450cda2f044d2ddc7b3a990
dwicahyo-dev <dwoppzxc@gmail.com>
2018-07-14 23:26:14 +0700

    Init Assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,26 +37,14 @@
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
     <!-- Scripts -->
+
     <!-- jQuery -->
     <script src="plugins/jquery/jquery.min.js"></script>
-    <!-- jQuery UI 1.11.4 -->
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
-    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-    <script>
-        $.widget.bridge('uibutton', $.ui.button)
-    </script>
     <!-- Bootstrap 4 -->
     <script src="{{ asset("plugins/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
     <!-- Morris.js charts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
     <script src="{{ asset("plugins/morris/morris.min.js") }}"></script>
-    <!-- Sparkline -->
-    <script src="{{ asset("plugins/sparkline/jquery.sparkline.min.js") }}"></script>
-    <!-- jvectormap -->
-    <script src="{{ asset("plugins/jvectormap/jquery-jvectormap-1.2.2.min.js") }}"></script>
-    <script src="{{ asset("plugins/jvectormap/jquery-jvectormap-world-mill-en.js") }}"></script>
-    <!-- jQuery Knob Chart -->
-    <script src="{{ asset("plugins/knob/jquery.knob.js") }}"></script>
     <!-- daterangepicker -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js"></script>
     <script src="{{ asset("plugins/daterangepicker/daterangepicker.js") }}"></script>
@@ -98,7 +86,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
+        <a href="{{ url('home') }}" class="brand-link">
             <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                  style="opacity: .8">
             <span class="brand-text font-weight-light">Titip.yuk</span>
@@ -429,7 +417,7 @@
                             <div class="inner">
                                 <h3>150</h3>
 
-                                <p>New Orders</p>
+                                <p>Goods</p>
                             </div>
                             <div class="icon">
                                 <i class="ion ion-bag"></i>
@@ -444,7 +432,7 @@
                             <div class="inner">
                                 <h3>53<sup style="font-size: 20px">%</sup></h3>
 
-                                <p>Bounce Rate</p>
+                                <p>Brands</p>
                             </div>
                             <div class="icon">
                                 <i class="ion ion-stats-bars"></i>
@@ -474,7 +462,7 @@
                             <div class="inner">
                                 <h3>65</h3>
 
-                                <p>Unique Visitors</p>
+                                <p>Categories</p>
                             </div>
                             <div class="icon">
                                 <i class="ion ion-pie-graph"></i>
@@ -541,7 +529,6 @@
 {{--{{ __('Logout') }}--}}
 {{--</a>--}}
 
-{{--<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
 {{--@csrf--}}
 {{--</form>--}}
 {{--</div>--}}
